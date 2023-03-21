15147e1270b66c462fcbd2a9df0d0722ea5bf931
denosaur98 <densichurchill1998@gmail.com>
2023-03-21 10:46:20 +0300

    sec

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -5,9 +5,31 @@ function Main() {
   return (
     <>
       <div className={css.main_container}>
-        <div className={css.bubbles_right}></div>
+        <div>
+          <div className={css.bubbles_right}></div>
+          <div className={css.bubbles_left}></div>
+        </div>
         <div className={css.line}></div>
         <p className={css.title}>Lorem ipsum, dolor sit amet consectetur</p>
+        <div className={css.pictures_container}>
+          <div className={css.operator}></div>
+          <div className={css.square}></div>
+          <p className={css.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          <p className={css.text2}>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className={css.pictures_container}>
+          <div className={css.note}></div>
+          <div className={css.berries}></div>
+          <div className={css.item}></div>
+          <p className={css.text3}>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          <p className={css.text4}>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className={css.pictures_container}>
+          <div className={css.operator2}></div>
+          <div className={css.square2}></div>
+          <p className={css.text5}>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          <p className={css.text6}>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        </div>
       </div>
     </>  
   )

0d14f923f0fc0df4c04526a202571aaa1b63cedb
denosaur98 <densichurchill1998@gmail.com>
2023-03-21 22:10:01 +0300

    header ok

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
-import css from '../header/Header.module.css'
+import React, { useState } from 'react';
+import css from '../header/Header.module.css';
 
 function Header() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleButtonClick = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <>
       <div className={css.header_container}>
@@ -9,9 +15,24 @@ function Header() {
           <p className={css.logo_text}>PRODUCT</p>
           <div className={css.logo}>24</div>
           <div className={css.btns_left_container}>
-            <button className={css.btns}>NavItem1</button>
-            <button className={css.btns}>NavItem2</button>
-            <button className={css.btns}>NavItem3</button>
+            <button
+              className={activeIndex === 0 ? css.btns : css.noact}
+              onClick={() => handleButtonClick(0)}
+            >
+              NavItem1
+            </button>
+            <button
+              className={activeIndex === 1 ? css.btns : css.noact}
+              onClick={() => handleButtonClick(1)}
+            >
+              NavItem2
+            </button>
+            <button
+              className={activeIndex === 2 ? css.btns : css.noact}
+              onClick={() => handleButtonClick(2)}
+            >
+              NavItem3
+            </button>
           </div>
         </div>
         <div className={css.btns_right_container}>

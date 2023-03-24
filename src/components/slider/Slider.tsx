4e7f1115302152f1852136b0723b2c14b2306a94
denosaur98 <densichurchill1998@gmail.com>
2023-03-24 14:15:59 +0300

    mobile_ok

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -5,11 +5,11 @@ function Slider(props) {
   const [activeIndex, setActiveIndex] = useState(0);
 
   const slideLeft = () => {
-    setActiveIndex((activeIndex - 1 + 4) % 4);
+    setActiveIndex((activeIndex - 1 + 5) % 5);
   };
   
   const slideRight = () => {
-    setActiveIndex((activeIndex + 1) % 4);
+    setActiveIndex((activeIndex + 1) % 5);
   };
 
   return (
@@ -17,22 +17,29 @@ function Slider(props) {
       <div className={css.carouselInner}>
         <div className={`${css.carouselItem} ${activeIndex === 0 ? css.active : ''}`}>
           <div className={css.operator}></div>
+          <p className={css.carouselText}>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
         </div>
         <div className={`${css.carouselItem} ${activeIndex === 1 ? css.active : ''}`}>
           <div className={css.square}></div>
+          <p className={css.carouselText}>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
         </div>
         <div className={`${css.carouselItem} ${activeIndex === 2 ? css.active : ''}`}>
           <div className={css.note}></div>
+          <p className={css.carouselText}>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
         </div>
         <div className={`${css.carouselItem} ${activeIndex === 3 ? css.active : ''}`}>
           <div className={css.item}></div>
+          <p className={css.carouselText}>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className={`${css.carouselItem} ${activeIndex === 4 ? css.active : ''}`}>
+          <div className={css.square}></div>
+          <p className={css.carouselText}>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
         </div>
       </div>
       <div className={css.arrows}>
         <button className={css.visuallyHidden} type="button" onClick={slideLeft}/>
         <button className={css.visuallyHiddenW} type="button" onClick={slideRight}/>
       </div>
-      
     </div>
   );
 }

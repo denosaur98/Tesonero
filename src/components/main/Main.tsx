17a2099ff3779115cb371ab0a8d9eccb91bd0183
denosaur98 <densichurchill1998@gmail.com>
2023-03-28 17:18:33 +0300

    mainOk

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -14,7 +14,7 @@ function Main() {
   return (
     <>
       <div className={css.main_container}>
-        <div className={css.berries}></div>,
+        <div className={css.berries}></div>
         {isSmallScreen ? (
           <Slider/>
         ) : (
